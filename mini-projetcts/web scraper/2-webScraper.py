ad88943cf1e22fe04737a39bdf605f1ee4bf6875
Filipe Rangel <Solarcubix@gmail.com>
2025-06-02 17:53:03 -0300

    feat: pandastesting

--- mini-projetcts/web scraper/2-webScraper.py
+++ mini-projetcts/web scraper/2-webScraper.py
@@ -1,7 +1,20 @@
 import scrapy
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+
+URL = "http://www.example.com"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = ['http://quotes.toscrape.com/tag/humor/',]
+    
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {'quote': quote.css('span.text::text').get()}
+
+
+driver = webdriver.Firefox()
+driver.get("http://www.example.com")
